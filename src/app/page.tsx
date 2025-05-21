0085f7e07eb4337241af014e7653bc724b1e0f0e
Steven Thomas <stevencthomas54@gmail.com>
2025-05-21 11:03:16 -0800

    Moved to typescript

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default async function Home() {
+
+  return (
+    <p>Hello, there!</p>
+  );
+}
--- src/database/services/userService.ts
+++ src/database/services/userService.ts
@@ -3,18 +3,18 @@ import User from "../models/user";
 import { connectToDatabase } from "@/database";
 
 /** Returns a read-only User object. */
-export async function getUserByEmailForRead(email) {
+export async function getUserByEmailForRead(email: string) {
   await connectToDatabase()
   return await User.findOne({email: email}).lean()
 }
 
 /** Returns an editable user object. Save using (object).save() */
-export async function getUserByEmailForUpdate(email) {
+export async function getUserByEmailForUpdate(email: string) {
   await connectToDatabase()
   return await User.findOne({email: email})
 }
 
-export async function getUserByIdForUpdate(userId) {
+export async function getUserByIdForUpdate(userId: string) {
   await connectToDatabase()
   if (isValidObjectId(userId)) {
     return await User.findOne({_id: userId})
@@ -22,7 +22,7 @@ export async function getUserByIdForUpdate(userId) {
   else throw new Error('Invalid user ID')
 }
 
-export async function updateUserEmail(userId, newEmail) {
+export async function updateUserEmail(userId: string, newEmail: string) {
   const user = await getUserByIdForUpdate(userId)
   user.email = newEmail
   await user.save()
