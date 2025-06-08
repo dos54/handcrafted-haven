1e20eeb37cbd1e66801670085689c6d4b7a5cf6a
Steven Thomas <stevencthomas54@gmail.com>
2025-06-07 17:50:13 -0800

    Added schema for comments

--- src/database/models/comment.ts
+++ src/database/models/comment.ts
@@ -1,7 +1,31 @@
+/*===============================================
+File: comment.ts
+Author: Steven Thomas
+Date: June 07, 2025
+Purpose: Schema for comments
+===============================================*/
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-  
-})
+  authorId: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  storyId: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Story'
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+},
+{
+  timestamps: true
+}
+)
 
 export default mongoose.models.Comment || mongoose.model('Comment', CommentSchema)
