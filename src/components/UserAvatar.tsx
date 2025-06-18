446082fc78135544c26525db1fae380873e994b9
Steven Thomas <stevencthomas54@gmail.com>
2025-06-18 10:38:15 -0800

    next is dumb

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -5,15 +5,13 @@ import { useSession } from "next-auth/react"
 
 export default function UserAvatar() {
   const { data: session } = useSession()
-  const image = session?.user?.image ?? "/images/handcrafted_haven_logo_black.png"
+  const image = session?.user?.image ?? "/images/handcrafted_haven_logo_black.svg"
 
   return (
     <div>
-      <Image
+      <img
         src={image}
         alt="User Avatar"
-        width={20}
-        height={20}
         className="w-20 h-20 rounded-full object-cover mx-auto"
       />
     </div>
