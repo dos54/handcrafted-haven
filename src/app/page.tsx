d9dd985e6d91582e79164962e456fef71a86d996
Steven Thomas <stevencthomas54@gmail.com>
2025-06-18 01:24:03 -0800

    Working on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-
+import SignIn from '@/components/sign-in';
 
 export default async function Home() {
   return (
     <main>
       <h1>Hello, there!</h1>
+      <SignIn />
     </main>
   );
 }
